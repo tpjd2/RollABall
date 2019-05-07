b9e0580f90f31f10e46ea1efbaa882e5de108154
SENACEDU\salmo.msjunior <salmo.sjunior@sp.senac.br>
2019-05-07 15:20:42 -0300

    Movimentação do Player e construção de paredes

---
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float velocidade;
+    private float movimentoH;
+    private float movimentoV;
+    private Vector3 movimento;
+    private Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movimentoH = Input.GetAxis("Horizontal");
+        movimentoV = Input.GetAxis("Vertical");
+
+        movimento = new Vector3(movimentoH, 0f, movimentoV);
+        rb.AddForce(movimento * velocidade);
+    }
+}
